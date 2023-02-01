cd0d4fbcfec094f94eaf2636562dd0cde2a81c6c
Igor Badyukov <badyukov1806302003@gmail.com>
2023-02-01 19:41:51 +0300

    feat: add methods in controllers and users dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { ArtistsController } from './artists/artists.controller';
-import { AlbumsController } from './albums/albums.controller';
-import { FavouritesController } from './favourites/favourites.controller';
+import { UsersController } from './entities/users/users.controller';
+import { ArtistsController } from './entities/artists/artists.controller';
+import { AlbumsController } from './entities/albums/albums.controller';
+import { FavouritesController } from './entities/favourites/favourites.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController, ArtistsController, AlbumsController, FavouritesController],
+  controllers: [
+    AppController,
+    UsersController,
+    ArtistsController,
+    AlbumsController,
+    FavouritesController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/entities/users/users.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getAll(): string {
+    return 'This action returns all users';
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: string): string {
+    return 'This action returns one user by id';
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return;
+  }
+
+  @Put(':id')
+  updateUser(@Body() updateUserDto: UpdateUserDto, @Param('id') id: string) {
+    return;
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: string) {
+    return;
+  }
+}

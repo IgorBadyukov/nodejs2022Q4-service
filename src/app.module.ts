bfc34a084661b0b5cd8340282d366a111f702b5e
Igor Badyukov <badyukov1806302003@gmail.com>
2023-02-01 17:04:14 +0300

    feat: add controllers for entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { ArtistsController } from './artists/artists.controller';
+import { AlbumsController } from './albums/albums.controller';
+import { FavouritesController } from './favourites/favourites.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UsersController, ArtistsController, AlbumsController, FavouritesController],
   providers: [AppService],
 })
 export class AppModule {}
